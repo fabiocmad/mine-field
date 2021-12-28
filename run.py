85bb9ffe20b942e0fafde5e70e3baee82841f86c
Fabio Madeira <fabio_madeira@outlook.com>
2021-12-28 12:23:44 +0000

    Adjust validations to check if all values provided are integers

--- run.py
+++ run.py
@@ -28,13 +28,22 @@ def how_to_play():
     Rules of the game that will be shown at beginning of each game
     Or whenever the user wants a reminder of the rules and how to play
     """
+    print("*********************** MINEFIELD - Rules of the game. ***********************\n")
+    print("The game begins with the board covered in tiles. Click on any tile to uncover it.")
+    print("Choose a column and a row respectively when prompted.")
+    print("If not a mine, a number of close mines will be shown. This will help you deduce where the mines are so that you can mark them")
+    print("Objectice is to clear the board without hitting any mines.")
+    print("If you hit a mine, game is over.")
+    
+    print("To see this message again, type 'help' at any time.\n")
+    print("********************************************************************************************")
     
 def get_player_move():
     """
     Get player movement choice on mine field
     """
     print("Please choose a column and a row, separated by comas. ")
-    print("Example: 3, 4")
+    print("Example: 3, 4\n")
     movement_data = input("Please choose: ")
 
     movement_data = movement_data.split(",")
@@ -48,6 +57,7 @@ def validate_data(values):
     or if theres no two values for columns and row
     """
     try:
+        [int(value) for value in values]
         if len(values) != 2:
             raise ValueError(
                 f'Two values are needed, you provided {len(values)}'
