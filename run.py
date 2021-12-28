9b40ece7e299ec16e6ec4b211505bef7bc7e3268
Fabio Madeira <fabio_madeira@outlook.com>
2021-12-28 11:19:46 +0000

    Create get_player_name function to get users name

--- run.py
+++ run.py
@@ -1,1 +1,26 @@
+import gspread
+from google.oauth2.service_account import Credentials
 
+SCOPE = [
+    "https://www.googleapis.com/auth/spreadsheets",
+    "https://www.googleapis.com/auth/drive.file",
+    "https://www.googleapis.com/auth/drive"
+    ]
+
+CREDS = Credentials.from_service_account_file('creds.json')
+SCOPED_CREDS = CREDS.with_scopes(SCOPE)
+GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
+SHEET = GSPREAD_CLIENT.open('mine_field')
+
+
+def get_player_name():
+    """
+    Get user name input
+    """
+    print("Hello player! Please add your name:")
+    print("Example: 'John', 'Michael', 'Bernard'")
+
+    data_str = input("Name: ")
+    print(f'The data provided is {data_str}')
+
+get_player_name()
