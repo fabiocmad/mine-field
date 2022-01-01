4bef5df2c504854b7ffec2d270397dad91d69bb0
Fabio Madeira <fabio_madeira@outlook.com>
2022-01-01 11:29:08 +0000

    Add validation for user level input

--- run.py
+++ run.py
@@ -14,7 +14,6 @@ mines_dict = {
     HARD: 10
 }
 
-EMPTY_SPACE = " "
 MINE = "*"
 
 SCOPE = [
@@ -57,46 +56,78 @@ def how_to_play():
     
     print("To see this message again, type 'help' at any time.\n")
     print("********************************************************************************************\n")
-    
+
 
 # Get user choice of difficulty level of the game
 def get_difficult_level(user_name):
-    print("Choose difficult level -> easy, medium or hard")
-    difficult_level = input(f'Hi {user_name}, please choose: ')
-    print("\n")
+    difficult_level = False
+
+    while not difficult_level:
+        difficult_level = input(f'Hi {user_name}, please choose difficult level -> easy, medium or hard: ')
+
+        if difficult_level == 'easy':
+            difficult_level = True
+            pass
+        elif difficult_level == 'medium':
+            difficult_level = True
+            pass
+        elif difficult_level == 'hard':
+            difficult_level = True
+            pass
+        else:
+            print("Invalid data, needs to be easy, medium or hard")
+            difficult_level = False
+            
+
+    # print("Choose difficult level -> easy, medium or hard")
+    # difficult_level = input(f'Hi {user_name}, please choose: ')
+  
+    # print("\n")
 
     return difficult_level
 
 # Generates a new map
 def generate_map(difficult_level):
-    matrix = [
-        [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
-        [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
-        [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
-        [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
-        [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
-        [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
-        [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE],
-        [EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE, EMPTY_SPACE]
+    empty_matrix = [
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "]
     ]
+    matrix = [
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "],
+        [" ", " ", " ", " ", " ", " ", " ", " "]
+    ]
+    
     for i in range(mines_dict.get(difficult_level, 5)):
         value_pos = None
         x = 0
         y = 0
-        while value_pos != EMPTY_SPACE:
+        while value_pos != " ":
             x = random.randint(0, 7)
             y = random.randint(0, 7)
             value_pos = matrix[x][y]
         
         matrix[x][y] = MINE
 
-    return matrix
+    return matrix, empty_matrix
 
 
 # User press enter to continue or to start the game
 def press_enter_to_start():
     input("Press enter to start the game.")
     # input() waits for a user input
+
 
 # Get player move to each of the times the player chooses their play
 def get_player_move(user_name):
@@ -123,6 +154,7 @@ def validate_data(values):
     Raises ValueError if they are not integers,
     or if theres no two values for columns and row
     """
+    # Put comments to exaplin here
     try:
         [int(value) for value in values]
         if len(values) != 2:
@@ -130,12 +162,12 @@ def validate_data(values):
                 f'Two values are needed, you provided {len(values)}'
             )
     except ValueError as e:
-        print(f'Invalid data: {e}, please try again.')
+        print(f"Seems like you typed {e}. It's an invalid value, please try again.")
         return False
 
     return True
 
-# Updating user input for each choice on the borad, onto the google sheet file
+
 def update_score_worksheet(data):
     """
     Update scores worksheet, add new row with the two numbers provided
@@ -150,20 +182,42 @@ def init_game():
     how_to_play()
     user_name = get_player_name()
     difficult_level = get_difficult_level(user_name)
-    map = generate_map(difficult_level)
+    map, empty_map = generate_map(difficult_level)
     press_enter_to_start()
     start_time_game = datetime.datetime.now()
     game_state = "progress"
 
-    return (user_name, difficult_level, start_time_game, game_state, map)
+    return (user_name, difficult_level, start_time_game, game_state, map, empty_map)
+
+
+def show_player_map(player_map):
+    pretty_map = ""
+    for row in player_map:
+        pretty_map += "-----------------\n"
+        pretty_row = "|"
+        for column in row:
+            pretty_row += (column + "|")
+        pretty_map += (pretty_row + "\n")
+    pretty_map += "-----------------\n"
+    print(pretty_map)
+
 
 # While game is not won neither lost, this loop will continue happening
-def game_loop(user_name, initial_game_state, map):
+def game_loop(user_name, initial_game_state, map, empty_map):
     game_state = initial_game_state
     
     while game_state == "progress":
+        show_player_map(empty_map)
         data = get_player_move(user_name)
+        print(data)
+        row, col = data
+        row = int(row)
+        col = int(col)
+        print("Found", map[row][col])
+        # if hit a bomb what happens
 
+        map[row][col] = "X"
+        empty_map[row][col] = "X"
 
     end_time_game = datetime.datetime()
     return (game_state, map, end_time_game)
@@ -180,8 +234,9 @@ def game_failed():
     game_state = "failed"
     pass
 
-(user_name, difficult_level, start_time_game, game_state, map) = init_game()
-(game_state, map, end_time_game) = game_loop(user_name, game_state, map)
+(user_name, difficult_level, start_time_game, game_state, map, empty_map) = init_game()
+
+(game_state, map, end_time_game) = game_loop(user_name, game_state, map, empty_map)
 
 if game_state == "failed":
     game_failed(user_name, difficult_level)
