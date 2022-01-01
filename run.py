ce055a4a141328e65174e99d2f0b5571c69462dd
Fabio Madeira <fabio_madeira@outlook.com>
2022-01-01 22:05:11 +0000

    Fix identation according to Pep8 requirements

--- run.py
+++ run.py
@@ -47,51 +47,48 @@ def how_to_play():
     Rules of the game that will be shown at beginning of each game
     Or whenever the user wants a reminder of the rules and how to play
     """
-    print("*********************** MINEFIELD - Rules of the game. ************************************\n")
+    print("******** MINEFIELD - Rules of the game. *********************\n")
     print("The game begins with the board covered in tiles.")
     print("Choose a column and a row respectively when prompted.")
-    print("If not a mine, a number of close mines will be shown. This will help you on where the mines are so that you can mark them")
     print("Objectice is to clear the board without hitting any mines.")
     print("If you hit a mine, game is over.")
-    
     print("To see this message again, type 'help' at any time.\n")
-    print("********************************************************************************************\n")
+    print("**************************************************************\n")
 
 
-
-def get_difficult_level(user_name):
+def get_dif_level(user_name):
     """
     Get user choice of difficulty level of the game and validate the data
     """
     difficult = False
 
     while not difficult:
-        difficult_level = input(f'Hi {user_name}, please choose difficult level -> easy, medium or hard: ')
+        print("Please choose difficult level -> easy, medium or hard:")
+        dif_level = input(f'Hi {user_name}, please choose: ')
 
-        if difficult_level == 'easy':
+        if dif_level == 'easy':
             difficult = True
-            difficult_level = EASY
+            dif_level = EASY
             print("\n")
             pass
-        elif difficult_level == 'medium':
+        elif dif_level == 'medium':
             difficult = True
-            difficult_level = MEDIUM
+            dif_level = MEDIUM
             print("\n")
             pass
-        elif difficult_level == 'hard':
+        elif dif_level == 'hard':
             difficult = True
-            difficult_level = HARD
+            dif_level = HARD
             print("\n")
             pass
         else:
             print("Invalid data, needs to be easy, medium or hard.\n")
             difficult = False
-            
-    return difficult_level
-    
+   
+    return dif_level
 
 
-def generate_map(difficult_level):
+def generate_map(dif_level):
     """
     Generates a new map
     """
@@ -115,8 +112,8 @@ def generate_map(difficult_level):
         [" ", " ", " ", " ", " ", " ", " ", " "],
         [" ", " ", " ", " ", " ", " ", " ", " "]
     ]
-    
-    for i in range(mines_dict.get(difficult_level, 5)):
+ 
+    for i in range(mines_dict.get(dif_level, 5)):
         value_pos = None
         x = 0
         y = 0
@@ -124,11 +121,10 @@ def generate_map(difficult_level):
             x = random.randint(0, 7)
             y = random.randint(0, 7)
             value_pos = matrix[x][y]
-        
+     
         matrix[x][y] = MINE
 
     return matrix, empty_matrix
-
 
 
 def press_enter_to_start():
@@ -143,7 +139,7 @@ def get_player_move(user_name):
     Get player movement choice on mine field
     """
     while True:
-        print(f"Hello {user_name}. Please choose a column and a row, separated by comas. ")
+        print(f"Hi {user_name}. Choose a col and a row, separated by comma.")
         print("Example: 3, 4\n")
         movement_data = input("Please choose your input or type 'help': ")
 
@@ -176,7 +172,7 @@ def validate_data(values):
                 f'Two values are needed, you provided {len(values)}'
             )
     except ValueError as e:
-        print(f"Seems like you typed {e}. It's an invalid value, please try again.")
+        print(f"Seems like you typed {e}. It's an invalid value, try again.")
         return False
 
     return True
@@ -198,13 +194,13 @@ def init_game():
     """
     how_to_play()
     user_name = get_player_name()
-    difficult_level = get_difficult_level(user_name)
-    map, empty_map = generate_map(difficult_level)
+    dif_level = get_dif_level(user_name)
+    map, empty_map = generate_map(dif_level)
     press_enter_to_start()
-    start_time_game = datetime.datetime.now()
+    st_time = datetime.datetime.now()
     game_state = "progress"
 
-    return (user_name, difficult_level, start_time_game, game_state, map, empty_map)
+    return (user_name, dif_level, st_time, game_state, map, empty_map)
 
 
 def restart_game():
@@ -213,12 +209,12 @@ def restart_game():
     """
     how_to_play()
     user_name = get_player_name()
-    difficult_level = get_difficult_level(user_name)
-    empty_map = generate_map(difficult_level)
+    dif_level = get_dif_level(user_name)
+    empty_map = generate_map(dif_level)
     press_enter_to_start()
     game_state = "progress"
 
-    return (user_name, difficult_level, game_state, empty_map)
+    return (user_name, dif_level, game_state, empty_map)
 
 
 def show_player_map(player_map):
@@ -235,13 +231,12 @@ def show_player_map(player_map):
     return (pretty_map)
 
 
-
 def game_loop(user_name, initial_game_state, map, empty_map):
     """
     While game is not won neither lost, this loop will continue happening
     """
     game_state = initial_game_state
-    
+
     while game_state == "progress":
         show_player_map(empty_map)
         data = get_player_move(user_name)
@@ -250,20 +245,23 @@ def game_loop(user_name, initial_game_state, map, empty_map):
         row = int(row)
         col = int(col)
         print("Found", map[row][col])
-        
+
         if map[row][col] == MINE:
             print("You hit a BOMB! You lost!")
             map[row][col] = "*"
             game_failed()
             break
 
+        else:
+            print("You won the game!")
+            game_completed()
+            break
+
         map[row][col] = "X"
         empty_map[row][col] = "X"
-        
 
-    
-    end_time_game = datetime.datetime.now()
-    return (game_state, map, end_time_game)
+    end_time = datetime.datetime.now()
+    return (game_state, map, end_time)
 
 
 def game_completed():
@@ -278,18 +276,8 @@ def game_failed():
     print("Thanks for playing!")
     print("--- End of the game --- type 'python3 run.py to restart' --- \n")
     pass
-    
-
-(user_name, difficult_level, start_time_game, game_state, map, empty_map) = init_game()
-
-(game_state, map, end_time_game) = game_loop(user_name, game_state, map, empty_map)
-
-if game_state == "failed":
-    game_failed(user_name, difficult_level)
-elif game_state == "completed":
-    duration = dateutil.relativedelta.relativedelta(end_time_game, start_time_game)
-    duration_text = "%d hours, %d minutes and %d seconds" % (duration.hours, duration.minutes, duration.seconds)
-    game_completed(user_name, difficult_level, duration_text)
 
 
+(user_name, dif_level, st_time, game_state, map, empty_map) = init_game()
 
+(game_state, map, end_time) = game_loop(user_name, game_state, map, empty_map)
